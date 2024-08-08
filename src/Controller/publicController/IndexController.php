87bb1cb2a1b492e28eb075bfa2e08d635c74827a
WilliamGendre <william.gendre@lapiscine.pro>
2024-08-08 18:47:57 +0200

    implantation scss

--- src/Controller/publicController/IndexController.php
+++ src/Controller/publicController/IndexController.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\Controller\PublicController;
+namespace App\Controller\publicController;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Attribute\Route;
