c4b1eb985e5f8f6f9b811b6ec22867592051de95
WilliamGendre <william.gendre@lapiscine.pro>
2024-08-27 11:39:50 +0200

    delete user by admin

---
+++ src/Controller/adminController/AdminUsersController.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\adminController;
+
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Attribute\Route;
+use Doctrine\ORM\EntityManagerInterface;
+
+class AdminUsersController extends AbstractController
+{
+
+    #[Route('admin/deleteUser/{id}', name: 'admin_deleteUser')]
+    public function deleteUser(int $id, UserRepository $userRepository, EntityManagerInterface $entityManager){
+        $user = $userRepository->find($id);
+
+        $entityManager->remove($user);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('index_admin', ['user' => $user->getId()]);
+    }
+}
